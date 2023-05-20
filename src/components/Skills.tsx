77a9e85b77fcc4227c0a947840fc5b71518a6cad
mikuweb <mxxx.kwtmg@gmail.com>
2023-05-20 12:49:19 +0900

    markup Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import { techs } from "@/constants/constants";
+import Image from "next/image";
+import React from "react";
 
 const Skills = () => {
   return (
     <div
-    id="skills"
-    className="border-2 h-auto max-w-4xl mx-auto flex flex-col items-center relative overflow-hidden"
-  >
-    <h2 className="font-bold text-5xl p-14 ">Skills</h2>
-    {/* eslint-disable-next-line react/no-unescaped-entities */}
-    <p>These are the technologies I've worked with</p>
+      id="skills"
+      className="border-2 h-auto max-w-4xl mx-auto flex flex-col items-center relative overflow-hidden"
+    >
+      <h2 className="font-bold text-5xl p-14 ">Skills</h2>
+      {/* eslint-disable-next-line react/no-unescaped-entities */}
+      <p className="mb-4">These are the technologies I've worked with</p>
+      <div className="border-2 w-4/5 mx-auto grid grid-cols-3 md:grid-cols-4 gap-8 text-center">
+        {techs.map(({ title, image, color, id }) => (
+          <div
+            key={id}
+            className={`shadow-md hover:scale-105 duration-200 py-2 rounded-lg ${color}`}
+          >
+            <Image src={image} alt={title} width={55} height={55} className="mx-auto" />
+            <p className="mt-2">{title}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -30,3 +30,66 @@ export const projects = [
     id: 2,
   },
 ];
+
+export const techs = [
+  {
+    title: "HTML",
+    image: "/images/html5_icon.svg",
+    color: "shadow-orange-500",
+    id: 0,
+  },
+  {
+    title: "CSS",
+    image: "/images/css3_icon.svg",
+    color: "shadow-sky-600",
+    id: 1,
+  },
+  {
+    title: "React",
+    image: "/images/react_icon.svg",
+    color: "shadow-cyan-500",
+    id: 2,
+  },
+  {
+    title: "Tailwind",
+    image: "/images/tailwind_icon.svg",
+    color: "shadow-cyan-500",
+    id: 3,
+  },
+  {
+    title: "Next.js",
+    image: "/images/nextjs_icon.svg",
+    color: "shadow-gray-400",
+    id: 4,
+  },
+  {
+    title: "TypeScript",
+    image: "/images/typescript_icon.svg",
+    color: "shadow-sky-600",
+    id: 5,
+  },
+  {
+    title: "Github",
+    image: "/images/github_icon.svg",
+    color: "shadow-gray-400",
+    id: 6,
+  },
+  {
+    title: "Node.js",
+    image: "/images/nodejs_icon.svg",
+    color: "shadow-green-600",
+    id: 7,
+  },
+  {
+    title: "Figma",
+    image: "/images/figma_icon.svg",
+    color: "shadow-pink-400",
+    id: 8,
+  },
+  {
+    title: "Sass/SCSS",
+    image: "/images/sass_icon.svg",
+    color: "shadow-pink-400",
+    id: 8,
+  },
+];
